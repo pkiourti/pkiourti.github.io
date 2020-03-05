a2fe4e619d5a4428f4632d46f9fee19dee72ccfa
Panayota Kiourti <pkiourti@gmail.com>
2020-03-05 16:30:33 -0500

    delete files

--- website/src/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as NavBar } from './navbar.js';
-export { default as ImgCard } from './imgcard.js';
-export { default as Intro } from './intro.js';
-export { default as Papers } from './papers.js';
