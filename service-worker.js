2086fb5ecded4eb93268537b5b2ec96770ab3167
Panayota Kiourti <pkiourti@gmail.com>
2020-03-06 22:45:43 -0500

    update website

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6f9a2627d68b6db231f0f9571ebebb56.js"
+  "/precache-manifest.98090c43d35909605ce0bda45f450167.js"
 );
 
 self.addEventListener('message', (event) => {
