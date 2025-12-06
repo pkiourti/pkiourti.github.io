1df08ddf7ae9653a4055f09b230cb8af99bf105c
pkiourti <pkiourti@gmail.com>
2025-12-06 07:11:35 +0000

    Deploying to gh-pages from @ pkiourti/pkiourti.github.io@d424b7196927a1e120eeed391f49e7c055fadbce 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -511,11 +511,25 @@ ninja.data = [{
           window.open("mailto:%70%6B%69%6F%75%72%74%69@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/pkiourti", "_blank");
+        },
+      },{
         id: 'social-inspire',
         title: 'Inspire HEP',
         section: 'Socials',
         handler: () => {
           window.open("https://inspirehep.net/authors/1010907", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/pkiourti", "_blank");
         },
       },{
         id: 'social-rss',
