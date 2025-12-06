34c0f8fde791e34bd4309db3c242d33c5ef2fffb
pkiourti <pkiourti@gmail.com>
2025-12-06 06:41:12 +0000

    Deploying to gh-pages from @ pkiourti/pkiourti.github.io@21d020668ea190f7a283185c3a865fd8cd7524a4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -508,7 +508,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%70%6B%69%6F%75%72%74%69@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -529,7 +529,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=ncU8YRsAAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
